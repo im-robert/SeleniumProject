94129fc90cd135064c2c9ab25a2a64ffdf15e8b2
Roberto Capellán <capellan1603@gmail.com>
2024-04-09 22:54:46 -0400

    Codigo actualizado

--- SeleniumProject/FacebookUnitTestingcs.cs
+++ SeleniumProject/FacebookUnitTestingcs.cs
@@ -11,34 +11,34 @@ namespace SeleniumProject
             IWebDriver driver = new ChromeDriver();
 
             //Primera prueba unitaria. Login de usuario con éxito
-            //LoginTesting.loginTest(driver);
+            LoginTesting.loginTest(driver);
 
-            //Thread.Sleep(8000);
+            Thread.Sleep(8000);
 
             //Segunda prueba unitaria. Búsqueda en la web
-            //driver = new ChromeDriver();
-            //SearchBarTest.SearchBarTesting(driver);
+            driver = new ChromeDriver();
+            SearchBarTest.SearchBarTesting(driver);
 
-            //Thread.Sleep(8000);
+            Thread.Sleep(8000);
 
             //Tercera prueba unitaria. Contraseña olvidada y metodos de recuperacion
-            //driver = new ChromeDriver();
-            //ForgottenPassword.ForgottenPasswordTest(driver);
+            driver = new ChromeDriver();
+            ForgottenPassword.ForgottenPasswordTest(driver);
 
-            //Thread.Sleep(8000);
+            Thread.Sleep(8000);
 
             //Cuarta prueba unitaria. Login de usuario fallido
-            //driver = new ChromeDriver();
-            //LoginFailed.LoginFailedTest(driver);
+            driver = new ChromeDriver();
+            LoginFailed.LoginFailedTest(driver);
 
-            //Thread.Sleep(8000);
+            Thread.Sleep(8000);
 
             //Quinta prueba unitaria. Visualizacion de lista de amigos
-            //driver = new ChromeDriver();
-            //FriendListTest.lookFriendList(driver);
+            driver = new ChromeDriver();
+            FriendListTest.lookFriendList(driver);
 
             //Sexta prueba unitaria. Enviar mensaje de texto
-            //driver = new ChromeDriver();
+            driver = new ChromeDriver();
             SendMessageTest.SendMessage(driver);
         }
     }
